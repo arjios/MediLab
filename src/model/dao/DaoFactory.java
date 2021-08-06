c865ebeb57b37322acee51ba6c364ae1bb4b3771
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-08-06 10:45:28 -0300

    Created DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package model.dao;
 
+import model.dao.impl.AnamneseDaoJSON;
+
 public class DaoFactory {
+	
+	public static AnamneseDao createAnamneseDao(String fileJSON) {
+		return new AnamneseDaoJSON(fileJSON);
+	}
 
 }
+
