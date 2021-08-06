dc1e6b64518fc33e92294bf2992ad23e98047a33
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-08-06 11:39:31 -0300

    Controller and pages

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,14 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Controller")
+@WebServlet(urlPatterns={"/Controller", "/anamnese"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
